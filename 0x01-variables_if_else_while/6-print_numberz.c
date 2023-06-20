4fab94216bc799e5ade6ea65d099f3861fcdad80
Rantiano <rantianoo@gmail.com>
2023-06-20 15:23:24 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers of base ten
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	int alphabets = '0';
+
+	while (alphabets <= '9')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+	putchar('\n');
+	return (0);
+}
